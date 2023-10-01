473779a8a6e04661f4cce4ea995766e78a541f30
Krishna Mohan Pandey <141332207+krishnnaa15@users.noreply.github.com>
2023-10-01 19:35:48 +0530

    Add files via upload

---
+++ word-list.js
@@ -0,0 +1,196 @@
+const wordList = [
+    {
+        word: "obvious",
+        hint: "easy to see, recognize, or understand"
+    },
+
+    {
+        word: "bluster",
+        hint: "to speak in a loud, angry, or offended way"
+    },
+
+    {
+        word: "weird",
+        hint: "very strange and unusual, unexpected, or not natural"
+    },
+
+    {
+        word: "vulnerable",
+        hint: "able to be easily physically or mentally hurt, influenced, or attacked"
+    },
+
+    {
+        word: "hexagon",
+        hint: "a flat shape with six straight sides"
+    },
+
+    {
+        word: "avoid",
+        hint: "to stay away from someone or something"
+    },
+
+    {
+        word: "request",
+        hint: "the act of politely or officially asking for something"
+    },
+
+    {
+        word: "reject",
+        hint: "to refuse to accept or use"
+    },
+
+    {
+        word: "zoology",
+        hint: "the scientific study of animals, especially their structure"
+    },
+
+    {
+        word: "junction",
+        hint: "a place where things, especially roads or railways, come together"
+    },
+
+    {
+        word: "squish",
+        hint: "to crush something that is soft"
+    },
+
+    {
+        word: "valentine",
+        hint: "someone you love or would like to have a romantic relationship with"
+    },
+
+    {
+        word: "displace",
+        hint: "to force something out of its usual or original position"
+    },
+
+    {
+        word: "unknown",
+        hint: "not known or familiar"
+    },
+
+    {
+        word: "jackpot",
+        hint: "the largest prize in a competition or game"
+    },
+
+    {
+        word: "dictionary",
+        hint: "a book that contains a list of words in alphabetical order and explains their meanings"
+    },
+
+    {
+        word: "anxiety",
+        hint: "an uncomfortable feeling of nervousness or worry about something"
+    },
+
+    {
+        word: "strategy",
+        hint: "a detailed plan for achieving success in any situation"
+    },
+
+    {
+        word: "precaution",
+        hint: "an action that is done to prevent something unpleasant or dangerous happening"
+    },
+
+    {
+        word: "consumer",
+        hint: "a person who buys goods or services for their own use"
+    },
+
+    {
+        word: "prefer",
+        hint: "to like, choose, or want one thing rather than another"
+    },
+
+    {
+        word: "scientist",
+        hint: "an expert who studies or works in one of the sciences"
+    },
+
+    {
+        word: "language",
+        hint: "a system of communication consisting of sounds, words, and grammar"
+    },
+
+    {
+        word: "introvert",
+        hint: "someone who is shy, quiet, and prefers to spend time alone" 
+    },
+
+    {
+        word: "hygiene",
+        hint: "conditions or practices conducive to maintaining health and preventing disease"
+    },
+
+    {
+        word: "doctor",
+        hint: "a person with a medical degree whose job is to treat people who are ill or hurt"
+    },
+
+    {
+        word: "handsome",
+        hint: "attractive, well-proportioned, good looking"
+    },
+
+    {
+        word: "future",
+        hint: "a period of time that is to come"
+    },
+
+    {
+        word: "miserable",
+        hint: "very unhappy, or causing much unhappiness"
+    },
+
+    {
+        word: "vegan",
+        hint: "a person who does not eat or use any animal products"
+    },
+
+    {
+        word: "explicit",
+        hint: "clear, making something easy to understand"
+    },
+
+    {
+        word: "certificate",
+        hint: "an official document that states that the information on it is true"
+    },
+
+    {
+        word: "anonymous",
+        hint: "made or done by someone whose name is not known"
+    },
+
+    {
+        word: "sketch",
+        hint: "a simple, quick drawing without many details"
+    },
+
+    {
+        word: "question",
+        hint: "a sentence or phrase used to find out information"
+    },
+
+    {
+        word: "licence",
+        hint: "an official document that gives you permission to own, do, or use something"
+    },
+
+    {
+        word: "emphasize",
+        hint: "to show that something is very important"
+    },
+
+    {
+        word: "comment",
+        hint: "something that you say or write that expresses your opinion"
+    },
+
+    {
+        word: "immediate",
+        hint: "occurring, acting, or accomplished without loss or interval of time"
+    }
+];
